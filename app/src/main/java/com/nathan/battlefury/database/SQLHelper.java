3ed55810acc852512d331b54a2cf3f935419a292
nathan <njgingrich@gmail.com>
2015-05-11 01:19:05 -0400

    First implementation of Cupboard

--- app/src/main/java/com/nathan/battlefury/database/SQLHelper.java
+++ app/src/main/java/com/nathan/battlefury/database/SQLHelper.java
@@ -17,16 +17,6 @@ public class SQLHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "matches.db";
 
-    /*private static final String SQL_DELETE =
-            "DROP TABLE IF EXISTS " + TableMatches.TABLE_NAME + ", " +
-                                      TablePlayers.TABLE_NAME + ", " +
-                                      TableAbilityUpgrades.TABLE_NAME;
-
-    private static final String SQL_CLEAR_TABLES =
-            "DELETE FROM " + TableMatches.TABLE_NAME + ";" +
-            "DELETE FROM " + TablePlayers.TABLE_NAME + ";" +
-            "DELETE FROM " + TableAbilityUpgrades.TABLE_NAME + ";";*/
-
     static {
         // register our models
         cupboard().register(Player.class);
@@ -52,72 +42,4 @@ public class SQLHelper extends SQLiteOpenHelper {
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         onUpgrade(db, oldVersion, newVersion);
     }
-
-    /*public void emptyTables(SQLiteDatabase db) {
-        Log.i("Database", "Emptied all tables.");
-        db.execSQL(SQL_CLEAR_TABLES);
-    }
-
-    public static abstract class TableMatches {
-        public static final String TABLE_NAME = "matches";
-
-        public static final String COL_PLAYER1  = "player1";
-        public static final String COL_PLAYER2  = "player2";
-        public static final String COL_PLAYER3  = "player3";
-        public static final String COL_PLAYER4  = "player4";
-        public static final String COL_PLAYER5  = "player5";
-        public static final String COL_PLAYER6  = "player6";
-        public static final String COL_PLAYER7  = "player7";
-        public static final String COL_PLAYER8  = "player8";
-        public static final String COL_PLAYER9  = "player9";
-        public static final String COL_PLAYER10 = "player10";
-
-        public static final String COL_RADIANT_WIN = "radiant_win";
-        public static final String COL_DURATION    = "duration";
-        public static final String COL_START_TIME  = "start_time";
-        public static final String COL_MATCH_ID    = "match_id";
-        public static final String COL_TOWERS_RADIANT = "towers_status_radiant";
-        public static final String COL_TOWERS_DIRE    = "towers_status_dire";
-        public static final String COL_BARRACKS_RADIANT = "barracks_status_radiant";
-        public static final String COL_BARRACKS_DIRE    = "barracks_status_dire";
-        public static final String COL_CLUSTER = "cluster";
-        public static final String COL_FIRST_BLOOD_TIME = "first_blood_time";
-        public static final String COL_LOBBY_TYPE       = "lobby_type";
-        public static final String COL_HUMAN_PLAYERS    = "human_players";
-        public static final String COL_GAME_MODE        = "game_mode";
-    }
-
-    public static abstract class TablePlayers {
-        public static final String TABLE_NAME = "match_players";
-
-        public static final String COL_MATCH_ID    = "match_id";
-        public static final String COL_ACCOUNT_ID  = "account_id";
-        public static final String COL_PLAYER_SLOT = "player_slot";
-        public static final String COL_HERO_ID     = "hero_id";
-        public static final String COL_ITEMS       = "items";
-        public static final String COL_KILLS       = "kills";
-        public static final String COL_DEATHS      = "deaths";
-        public static final String COL_ASSISTS     = "assists";
-        public static final String COL_GOLD        = "gold";
-        public static final String COL_LAST_HITS   = "last_hits";
-        public static final String COL_DENIES      = "denies";
-        public static final String COL_GPM         = "gpm";
-        public static final String COL_XPM         = "xpm";
-        public static final String COL_GOLD_SPENT  = "gold_spent";
-        public static final String COL_HERO_DMG    = "hero_damage";
-        public static final String COL_TOWER_DMG   = "tower_damage";
-        public static final String COL_HERO_HEAL   = "hero_healing";
-        public static final String COL_LEVEL       = "level";
-        public static final String COL_UPGRADES    = "upgrades";
-    }
-
-    public static abstract class TableAbilityUpgrades {
-        public static final String TABLE_NAME = "abilityUpgrades";
-
-        public static final String COL_MATCH_ID   = "match_id";
-        public static final String COL_ACCOUNT_ID = "account_id";
-        public static final String COL_ABILITY    = "ability";
-        public static final String COL_TIME       = "time";
-        public static final String COL_LEVEL      = "level";
-    }*/
 }
